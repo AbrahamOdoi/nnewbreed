042825b8d525842fd34c03d3c9c5f9ba6f031bd5
AbrahamOdoi <abrahamodoi2002@gmail.com>
2016-10-20 09:46:51 +0100

    9

--- scripts/index.js
+++ scripts/index.js
@@ -63,6 +63,7 @@
 		var element = document.getElementById('displayPositionWatch');
 		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
 	}
+	
 	function onError(error) {
 		alert('code ' + error.code + '\n' + 'message: ' + error.message + '\n');
 	}
